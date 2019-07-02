0068f7a3f40edbca8118fb6509fa35d8f87b1b32
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-07-02 21:50:31 +0000

    T7: update all class

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ import cmd
 import models
 from models.base_model import BaseModel
 
+
 class HBNBCommand(cmd.Cmd):
     """ HBNBCommand - contains the entry point of the command interprete
     attributes: prompt
@@ -61,14 +62,14 @@ class HBNBCommand(cmd.Cmd):
                 id_to_check = args_list[0] + "." + args_list[1]
                 all_objects = models.storage.all()
 
-                if not id_to_check in all_objects:
+                if id_to_check not in all_objects:
                     print("** no instance found **")
                 else:
                     print(all_objects[id_to_check])
 
     def do_destroy(self, args):
-        """Prints the string representation of an instance based on the class
-        name and id
+        """ Deletes an instance based on the class name and id (save the change
+        into the JSON file).
         """
         args_list = args.split()
         list_class = ["BaseModel"]
@@ -88,12 +89,98 @@ class HBNBCommand(cmd.Cmd):
                 id_to_check = args_list[0] + "." + args_list[1]
                 all_objects = models.storage.all()
 
-                if not id_to_check in all_objects:
+                if not id_to_check not in all_objects:
                     print("** no instance found **")
                 else:
                     del all_objects[id_to_check]
                     models.storage.save()
-                    models.storage.reload()
+
+    def do_all(self, args):
+        """ Prints all string representation of all instances based or not on
+        the class name.
+        """
+        args_list = args.split()
+        list_class = ["BaseModel"]
+        all_objects = models.storage.all()
+        list_of_print = []
+
+        if len(args_list) == 0:
+            for key, value in all_objects.items():
+                list_of_print.append(value.__str__())
+            print(list_of_print)
+        else:
+            if not args_list[0] in list_class:
+                print("** class doesn't exist **")
+            else:
+                for key, value in all_objects.items():
+                    if value.to_dict()["__class__"] == args_list[0]:
+                        list_of_print.append(value.__str__())
+                print(list_of_print)
+
+    def do_update(self, args):
+        """Updates an instance based on the class name and id by adding or
+        updating attribute (save the change into the JSON file).
+        """
+        if '"' in args:
+            args_list_list = args.split('"')  # args[0] | args[1]
+            args_list = args_list_list[0].split()
+            args_list.append(args_list_list[1].split('"')[0])
+        else:
+            args_list = args.split()
+
+        list_class = ["BaseModel"]
+
+        if len(args_list) == 0:  # $Update
+            print("** class name missing **")
+        elif len(args_list) == 1:  # 1.<class name>
+            if not args_list[0] in list_class:  # $Update MyModel
+                print("** class doesn't exist **")
+            else:  # $ update BaseModel
+                print("** instance id missing **")
+        elif len(args_list) == 2:  # 2.<id>
+            if args_list[0] not in list_class:  # $Update MyModel
+                print("** class doesn't exist **")
+            else:
+                id_to_check = args_list[0] + "." + args_list[1]
+                all_objects = models.storage.all()
+
+                if id_to_check not in all_objects:  # $update BaseModel 121
+                    print("** no instance found **")
+                else:  # $ update BaseModel existing-id
+                    print("** attribute name missing **")
+        elif len(args_list) == 3:  # 3.<attribute name>
+            if not args_list[0] in list_class:  # $Update MyModel
+                print("** class doesn't exist **")
+            else:
+                id_to_check = args_list[0] + "." + args_list[1]
+                all_objects = models.storage.all()
+
+                if id_to_check not in all_objects:  # $update BaseModel 121212
+                    print("** no instance found **")
+                else:  # $ update BaseModel existing-id
+                    print("** value missing **")  # $ update BaseModel id
+        else:  # 4."<attribute value>"
+            if not args_list[0] in list_class:  # $Update MyModel
+                print("** class doesn't exist **")
+            else:
+                id_to_check = args_list[0] + "." + args_list[1]
+                all_objects = models.storage.all()
+
+                if id_to_check not in all_objects:  # $update BaseModel 121212
+                    print("** no instance found **")
+                else:  # $ update BaseModel existing-id
+                    value_temp = args_list[3]
+
+                    try:
+                        if "." in value_temp:  # chrek if is posible float
+                            value = float(value_temp)
+                        else:
+                            value = int(value_temp)  # check if is posible int
+                    except:
+                        value = value_temp  # assign value how string
+
+                    setattr(all_objects[id_to_check], args_list[2], value)
+                    models.storage.save()
 
 if __name__ == "__main__":
     mycmd = HBNBCommand()
