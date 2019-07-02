9a03f10ceee0d3ca89946f47b7ae9ae011e92f70
Diegokernel <777@holbertonschool.com>
2019-07-01 20:46:34 -0500

    instances

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''module for city'''
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    '''City class that inherits from BaseModel class'''
+    state_id = ''
+    name = ''
