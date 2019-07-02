128c1807c77f3488c60f99faf0448bc9d931b4a8
= <=>
2019-07-02 17:37:41 +0000

    fix test

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -23,43 +23,43 @@ class test_user(unittest.TestCase):
         except BaseException:
             pass
 
-    def pep8_test_style(self):
+    def test_pep8_test_style(self):
         '''Pep8 style test'''
         pepe = pep8.StyleGuide(quiet=True)
         res = style.check_files(['models/review.py'])
         self.assertEqual(res.total_errors, 0, "Fix Style")
 
-    def docstring_class_class(self):
+    def test_docstring_class_class(self):
         self.assertIsNotNone(Review.__doc__)
 
-    def check_if_hasattr(self):
+    def test_check_if_hasattr(self):
         '''Checks if the methods exists'''
         self.assertTrue(hasattr(Review, "place_id"))
         self.assertTrue(hasattr(Review, "text"))
         self.assertTrue(hasattr(Review, "user_id"))
 
-    def constructor_test(self):
+    def test_constructor_test(self):
         '''Tests for the constructor'''
         self.assertTrue(isinstance(self.review1, BaseModel))
 
-    def save_method_test(self):
+    def test_save_method_test(self):
         '''Tests save method'''
         self.review1.save()
         self.assertNotEqual(self.review1.created_at, self.review1.updated_at)
 
-    def id_fun_test(self):
+    def test_id_fun_test(self):
         """ test id functionality """
         self.assertEqual(str, type(self.review.id))
 
-    def created_at_fun_test(self):
+    def test_created_at_fun_test(self):
         """ test created_at functionality"""
         self.assertEqual(datetime, type(self.review.created_at))
 
-    def updated_at_fun_test(self):
+    def test_updated_at_fun_test(self):
         """ test updated_at functionality"""
         self.assertEqual(datetime, type(self.review.updated_at))
 
-    def dictionary_test(self):
+    def test_dictionary_test(self):
         '''Tests to_dict method'''
         test_dict = self.review.to_dict()
         self.assertEqual(type(test_dict), dict)
