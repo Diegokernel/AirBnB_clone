bee920be5f61a7665c398874474c0573d52ade98
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-07-02 22:02:36 +0000

    T6: add emptyline

--- console.py
+++ console.py
@@ -10,7 +10,7 @@ from models.base_model import BaseModel
 class HBNBCommand(cmd.Cmd):
     """ HBNBCommand - contains the entry point of the command interprete
     attributes: prompt
-    methods: do_quit, do_EOF.
+    methods: do_quit, do_EOF, create
     """
     prompt = "(hbnb) "
 
