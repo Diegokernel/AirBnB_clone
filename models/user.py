78d54bf951a3d5f21c86136e3bb6de6e86fcf9fe
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-07-07 04:13:16 +0000

    T16: test update clase User

--- models/user.py
+++ models/user.py
@@ -4,8 +4,15 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """class User"""
+
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """__init__ - Method initializer of instance
+        args: self
+        return: nothing
+        """
+        super().__init__(self, *args, **kwargs)
